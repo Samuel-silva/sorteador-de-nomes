6059e96ae88f3d1150267c9d540527f8bbf8afb5
Samuel Barbosa <samuca192silva@hotmail.com>
2023-08-17 17:06:11 -0300

    feat - Optimization CSS file em PROD mode

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,6 +1,40 @@
 const common = require('./webpack.common.config.js')
 const { merge } = require('webpack-merge')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    filename: 'js/[name].[contenthash:12].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:12].css',
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+    ],
+  },
 })
