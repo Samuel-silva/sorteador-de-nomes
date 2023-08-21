c0267a08784615fbef6a39155959a694ef6251bf
Samuel Barbosa <samuca192silva@hotmail.com>
2023-08-21 16:03:11 -0300

    feat - Changing img css by sass

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import HtmlIcon from '../images/html.png';
 const htmlImg = document.querySelector('.title__html-image');
 const img = document.createElement('img');
 img.src = HtmlIcon;
+img.alt = 'HTML'
 htmlImg.appendChild(img);
 
 console.log('Hello world!');
