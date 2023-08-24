1edd3347683b32d81a305611002a86c3460faef7
Samuel Barbosa <samuca192silva@hotmail.com>
2023-08-24 16:41:10 -0300

    feat - Add modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,9 @@
 import '../styles/app.scss';
 import HtmlIcon from '../images/html.png';
+import { Modal } from 'bootstrap'
 
 const htmlImg = document.querySelector('.title__html-image');
 const img = document.createElement('img');
 img.src = HtmlIcon;
 img.alt = 'HTML'
 htmlImg.appendChild(img);
-
-console.log('Hello world!');
